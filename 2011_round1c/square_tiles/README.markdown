d7ae68022738b1976744969a3701ce0bcca8ce97
Clif Reeder <clif@sbnation.com>
2012-05-03 23:04:20 -0400

    added min_scalar_product practice; passes large and small

--- 2011_round1c/square_tiles/README.markdown
+++ 2011_round1c/square_tiles/README.markdown
@@ -1,6 +1,6 @@
 #Square Tiles
 
 Completed as practice. Took 45 minutes, passed small and large input on
-first truel
+first try.
 
 [Link to problem description](http://code.google.com/codejam/contest/1128486/dashboard)
--- min_scalar_product/min_scalar_product.rb
+++ min_scalar_product/min_scalar_product.rb
@@ -1,4 +1,14 @@
 ARGF.readline
+count = 0
 ARGF.each do |input|
-  puts input
+  count += 1
+  x = ARGF.readline.split.map(&:to_i)
+  y = ARGF.readline.split.map(&:to_i)
+  x.sort!
+  y.sort!.reverse!
+  sum = 0
+  x.size.times do |i|
+    sum += x[i] * y[i]
+  end
+  puts "Case ##{count}: #{sum}"
 end
